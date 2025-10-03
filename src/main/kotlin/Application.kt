e1e8aaf131aecec71425819fd7dd9bcf35b10d36
Amos-Tech-code <amosk51332@gmail.com>
2025-10-03 13:38:32 +0300

    Updating Application.kt
    Signed-off-by: Amos-Tech-code <amosk51332@gmail.com>

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,26 +1,11 @@
 package com.amos_tech_code
 
-import com.amos_tech_code.plugins.configureAdministration
-import com.amos_tech_code.plugins.configureAuthentication
-import com.amos_tech_code.plugins.configureDatabase
-import com.amos_tech_code.plugins.configureExceptionHandler
-import com.amos_tech_code.plugins.configureFrameworks
-import com.amos_tech_code.plugins.configureHTTP
-import com.amos_tech_code.plugins.configureMonitoring
-import com.amos_tech_code.plugins.configureRouting
-import com.amos_tech_code.plugins.configureSerialization
-import io.github.cdimascio.dotenv.dotenv
+import com.amos_tech_code.plugins.*
 import io.ktor.server.application.*
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
 
 fun main(args: Array<String>) {
 
-    val env = dotenv()
-    val serverPort = env["SERVER_PORT"]?.toIntOrNull() ?: 8443
-
-    embeddedServer(Netty, port = serverPort, module = Application::module)
-        .start(wait = true)
+    io.ktor.server.netty.EngineMain.main(args)
 
 }
 
